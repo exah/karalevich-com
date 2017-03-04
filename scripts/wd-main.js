a23a9cb306cef16299e7deb7d46bbb44fb6367a0
Maksim K <hi@karalevich.com>
2017-03-04 13:38:49 +0300

    WIP: Specials fix

--- scripts/wd-main.js
+++ scripts/wd-main.js
@@ -1,88 +1,43 @@
 $(document).ready(function() {
 
+  // Click on Invitation
+
   $('.wd__open--button').click(function() {
-    $('#wd__intro').show();
     $('#wd__info').show();
     $('#wd__open').hide();
   })
 
-  // menu switcher
+  // Menu switcher
 
   $('#wd__guests, #wd__where, #wd__timing, #wd__lookbook').click(function() {
     $(this).siblings().removeClass('wd__menu-item--active');
     $(this).addClass('wd__menu-item--active');
   })
 
-  // content revealing
+  $('#wd__heart').click(function() {
+    $('#wd__guests, #wd__where, #wd__timing, #wd__lookbook').removeClass('wd__menu-item--active');
+  })
+
+  // Content revealing
 
   $('#wd__guests').click(function() {
-    $('.wd__guests').siblings().hide();
-    $('.wd__guests').show();
-    $('#wd__info').css({
-      'background': ('#FFF3F3')
+    $('#wd__guests-screen').siblings().addClass('wd__display--none');
+    $('#wd__guests-screen').removeClass('wd__display--none')
     })
-  })
 
   $('#wd__where').click(function() {
-    $('.wd__where').siblings().hide();
-    $('.wd__where').show();
-    $('#wd__info').css({
-      'background': ('#F7FFF5')
+    $('#wd__where-screen').siblings().addClass('wd__display--none');
+    $('#wd__where-screen').removeClass('wd__display--none')
     })
-  })
-
-  $('#wd__timing').click(function() {
-    $('.wd__timing').siblings().hide();
-    $('.wd__timing').show();
-    $('#wd__info').css({
-      'background': ('#F8FFFE')
-    })
-  })
 
   $('#wd__lookbook').click(function() {
-    $('.wd__lookbook').siblings().hide();
-    $('.wd__lookbook').show();
-    $('#wd__info').css({
-      'background': ('#FFFEE6')
+    $('#wd__lookbook-screen').siblings().addClass('wd__display--none');
+    $('#wd__lookbook-screen').removeClass('wd__display--none')
     })
-  })
 
-  // Scroll to Info
-
-  $('#wd__intro').click(function() {
-    $('html, body').animate({
-      scrollTop: $('#wd__info').offset().top
-    }, 600);
-  })
-
-  $(function() {
-    $('.wd__envelope').draggable();
-  })
-
-  // Fade out Intro
-
-  $(window).scroll(function() {
-
-    var scrollTop = $(window).scrollTop();
-    var height = ( $(window).height() - 100);
-
-    $('#wd__intro').css({
-        'opacity': ((height - scrollTop) / height)
-    });
-  });
-
-  // Fixed menu
-
-  $(window).scroll(function() {
-
-    var scrollTop = $(window).scrollTop();
-    var menuHeight = $(window).height();
-
-    if (scrollTop > menuHeight) {
-      $('.wd__menu').addClass('wd__menu--fixed');
-    } else {
-      $('.wd__menu').removeClass('wd__menu--fixed');
-    }
-  })
+  $('#wd__heart').click(function() {
+    $('#wd__message-screen').siblings().addClass('wd__display--none');
+    $('#wd__message-screen').removeClass('wd__display--none')
+    })
 
 })
